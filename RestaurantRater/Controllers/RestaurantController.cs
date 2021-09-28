f9fea148b13f704350c009d224c559bf14a5ac94
Dinara Abakirova <abakirovada@gmail.com>
2021-09-28 10:07:49 -0400

    Delete Functionality Added

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,10 +11,53 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : Controller
     {
         private RestaurantDbContext _db= new RestaurantDbContext();
-        // GET: Restaurant
+        // GET: Restaurant/Index
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+        //Get: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Restaurant/Create
+        [HttpPost]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+        //Get: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //Post:Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
